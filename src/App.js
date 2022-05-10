e9c8255f84739fe2e5a9e65b1054523b6ed88418
Muhammad Talha Butt <63363869+talhabutt1996@users.noreply.github.com>
2022-05-10 13:05:33 +0100

    change state using hoocks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import './App.css'
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Person from './Person/Person'
-class App extends Component {
-  state = {
+const App = props => {
+  const [PersenState, setPerenState] = useState({
     persons: [
-      { name: "Muhammad", age: 26 },
+      { name: "Talha", age: 26 },
       { name: "Tayyab", age: 27 },
-      { name: "ali", age: 29 }
+      { name: "ali", age: 25 }
     ]
-  }
-  render() {
-    return (
-      <div className='App'>
-        <h1>hello i'm react</h1>
-        <p> This is really working</p>
-        <button>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Gamming</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-      </div>
-    )
-  }
-}
+  })
 
+  const switchButtonHandler = () => {
+    // console.log("clicked")
+    setPerenState({
+      persons: [
+        { name: "Muhammad", age: 26 },
+        { name: "Tayyab", age: 27 },
+        { name: "ali", age: 22 }
+      ]
+    })
+  }
+  return (
+    <div className='App'>
+      <h1>hello i'm react</h1>
+      <p> This is really working</p>
+      <button onClick={switchButtonHandler}>Switch Name</button>
+      <Person name={PersenState.persons[0].name} age={PersenState.persons[0].age} />
+      <Person name={PersenState.persons[1].name} age={PersenState.persons[1].age}>My Hobbies: Gamming</Person>
+      <Person name={PersenState.persons[2].name} age={PersenState.persons[2].age} />
+    </div>
+  )
+}
 export default App
