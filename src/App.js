06889d809be8f4ba2afbf7b6012135d3f31aaf4b
Muhammad Talha Butt <63363869+talhabutt1996@users.noreply.github.com>
2022-06-16 11:29:20 +0100

    print data using list

--- src/App.js
+++ src/App.js
@@ -46,12 +46,9 @@ class App extends Component {
     if (this.state.showPerson) {
       persons = (
         <div>
-          <Person name={this.state.persons[0].name}
-            age={this.state.persons[0].age} changed={this.changeHandler} />
-          <Person name={this.state.persons[1].name}
-            age={this.state.persons[1].age}>My Hobbies: Gamming</Person>
-          <Person name={this.state.persons[2].name}
-            age={this.state.persons[2].age} click={this.switchButtonHandler.bind(this, 'Farzana')} />
+          {this.state.persons.map(person => {
+            return <Person name={person.name} age={person.age} />
+          })}
         </div>
       )
     }
