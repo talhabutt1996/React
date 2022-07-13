bf5b278ab78ba5ebaba4120225322cba943705bf
Muhammad Talha Butt <63363869+talhabutt1996@users.noreply.github.com>
2022-07-13 15:18:09 +0100

    use Context Hooks

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -8,7 +8,6 @@ class Person extends Component {
             <Fragment>
 
                 {this.context.Authenticated ? <p>Authenticated</p> : <p>Please Login</p>}
-
                 <p onClick={this.props.click}> i 'm {this.props.name} and i'm {this.props.age} year old</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name} />
